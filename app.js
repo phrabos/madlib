51f41215e96531c40739b00a1017ceb91dc2629c
Patrick Hrabos <phrabos@gmail.com>
2020-12-07 12:42:56 -0500

    adds CSS styling, blank html elements, js selectors

---
+++ app.js
@@ -0,0 +1,7 @@
+// selectors
+const noun = document.getElementById('noun');
+const nounInputBox = document.getElementById("noun-input");
+const submitButton = document.getElementById("submit-button");
+
+//event listener
+

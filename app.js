0963a5d1924a1041a9a6fdbb1054366707b8dc20
Patrick Hrabos <phrabos@gmail.com>
2020-12-08 15:26:13 -0500

    fixes color input box functionality, moves script src tag into html body, fixes input selectors

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // selectors
 const noun = document.getElementById('noun1');
+const nounTwo = document.getElementById('noun2');
 const nounInputBox = document.getElementById("noun-input");
 const submitButton = document.getElementById("submit-button");
-
-
-const color = document.getElementById('color1');
 const colorInputBox = document.getElementById("color1-input");
 const verb = document.getElementById('verb1');
 const verbInputBox = document.getElementById('verb1-input');
+const body = document.getElementById('body');
 
 
 
@@ -19,9 +18,11 @@ const verbInputBox = document.getElementById('verb1-input');
 submitButton.addEventListener('click', () => {
      noun.textContent = nounInputBox.value;
      nounTwo.textContent = nounInputBox.value;
-     nounInputBox.value='';
      verb.textContent = verbInputBox.value;
-     color.textContent = colorInputBox.value;
+     body.style.backgroundColor = colorInputBox.value;
+     nounInputBox.value='';
+     verbInputBox.value='';
+     colorInputBox.value='';
 
 })
 

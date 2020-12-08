d06337d6136fc317062e5d1b2fd722bcd64b148e
Anthony Rosario <anthonymrosario225@gmail.com>
2020-12-08 11:06:35 -0800

    added input values/changed font/made input fields grow with text typed

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // selectors
-const noun = document.getElementById('noun');
+const noun = document.getElementById('noun1');
 const nounInputBox = document.getElementById("noun-input");
 const submitButton = document.getElementById("submit-button");
-const nounTwo = document.getElementById('noun-2');
+
+
+const color = document.getElementById('color1');
+const colorInputBox = document.getElementById("color1-input");
+const verb = document.getElementById('verb1');
+const verbInputBox = document.getElementById('verb1-input');
+
+
+
+
+
+
 //event listener
 
 submitButton.addEventListener('click', () => {
      noun.textContent = nounInputBox.value;
-     nounTwo.textContent = nounInputBox.value;
+     verb.textContent = verbInputBox.value;
+     color.textContent = colorInputBox.value;
 
 })
-
 

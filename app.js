84a141ec9779b842966f2198edd9d2b8fad271b5
Patrick Hrabos <phrabos@gmail.com>
2020-12-08 16:30:41 -0500

    adds color dropdown list and transparency css toggle for haiku body

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const noun = document.getElementById('noun1');
 const nounTwo = document.getElementById('noun2');
 const nounInputBox = document.getElementById("noun-input");
 const submitButton = document.getElementById("submit-button");
-const colorInputBox = document.getElementById("color1-input");
+const colorInputBox = document.getElementById("color-input");
 const verb = document.getElementById('verb1');
 const verbInputBox = document.getElementById('verb1-input');
-const body = document.getElementById('body');
-
-
-
-
-
+const image = document.getElementById('image');
+const transparent = document.getElementById('transparent');
 
 //event listener
 
@@ -19,7 +15,8 @@ submitButton.addEventListener('click', () => {
      noun.textContent = nounInputBox.value;
      nounTwo.textContent = nounInputBox.value;
      verb.textContent = verbInputBox.value;
-     body.style.backgroundColor = colorInputBox.value;
+     image.style.backgroundColor = colorInputBox.value;
+     transparent.classList.toggle('visible');
      nounInputBox.value='';
      verbInputBox.value='';
      colorInputBox.value='';
